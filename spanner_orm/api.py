b636fca6f9685aa027d57ae3d3dc2f081ef70f9b
Derek Brandao <dbrandao@google.com>
2019-03-14 19:43:56 +0000

    Add annotations to Spanner API classes

--- spanner_orm/api.py
+++ spanner_orm/api.py
@@ -14,14 +14,22 @@
 # limitations under the License.
 """Class that handles API calls to Spanner."""
 
+from __future__ import annotations
+
 import abc
 import logging
+from typing import Any, Callable, Iterable, List, Optional, TypeVar
 
 from spanner_orm import error
 
+from google.auth import credentials as auth_credentials
 from google.cloud import spanner
+from google.cloud.spanner_v1 import database as spanner_database
+from google.cloud.spanner_v1 import transaction as spanner_transaction
 
 _logger = logging.getLogger(__name__)
+
+CallableReturn = TypeVar('CallableReturn')
 
 
 class SpannerReadApi(abc.ABC):
@@ -29,18 +37,21 @@ class SpannerReadApi(abc.ABC):
 
   @classmethod
   @abc.abstractmethod
-  def _connection(cls):
+  def _connection(cls) -> spanner_database.Database:
     raise NotImplementedError
 
   @classmethod
-  def run_read_only(cls, method, *args, **kwargs):
+  def run_read_only(cls, method: Callable[..., CallableReturn], *args: Any,
+                    **kwargs: Any) -> CallableReturn:
     """Wraps read-only queries in a read transaction."""
     with cls._connection().snapshot(multi_use=True) as snapshot:
       return method(snapshot, *args, **kwargs)
 
   # Read methods
   @staticmethod
-  def find(transaction, table_name, columns, keyset):
+  def find(transaction: spanner_transaction.Transaction, table_name: str,
+           columns: Iterable[str],
+           keyset: spanner.KeySet) -> List[Iterable[Any]]:
     """Obtains rows with primary_keys from the given table."""
     _logger.debug('Find table=%s columns=%s keys=%s', table_name, columns,
                   keyset.keys)
@@ -49,7 +60,9 @@ class SpannerReadApi(abc.ABC):
     return list(stream_results)
 
   @staticmethod
-  def sql_query(transaction, query, parameters, parameter_types):
+  def sql_query(transaction: spanner_transaction.Transaction, query: str,
+                parameters: Iterable[str],
+                parameter_types: Iterable[Any]) -> List[Iterable[Any]]:
     """Runs a read only SQL query."""
     _logger.debug('Executing SQL:\n%s\n%s\n%s', query, parameters,
                   parameter_types)
@@ -63,36 +76,41 @@ class SpannerWriteApi(abc.ABC):
 
   @classmethod
   @abc.abstractmethod
-  def _connection(cls):
+  def _connection(cls) -> spanner_database.SpannerDatabase:
     raise NotImplementedError
 
   @classmethod
-  def run_write(cls, *args, **kwargs):
+  def run_write(cls, method: Callable[..., CallableReturn], *args: Any,
+                **kwargs: Any) -> CallableReturn:
     """Wraps write and read-write queries in a transaction."""
-    return cls._connection().run_in_transaction(*args, **kwargs)
+    return cls._connection().run_in_transaction(method, *args, **kwargs)
 
   @staticmethod
-  def delete(transaction, table_name, keyset):
+  def delete(transaction: spanner_transaction.Transaction, table_name: str,
+             keyset: spanner.KeySet) -> None:
     _logger.debug('Delete table=%s keys=%s', table_name, keyset.keys)
     transaction.delete(table=table_name, keyset=keyset)
 
   # Write methods
   @staticmethod
-  def insert(transaction, table_name, columns, values):
+  def insert(transaction: spanner_transaction.Transaction, table_name: str,
+             columns: Iterable[str], values: Iterable[Iterable[Any]]) -> None:
     """Add rows to a table."""
     _logger.debug('Insert table=%s columns=%s values=%s', table_name, columns,
                   values)
     transaction.insert(table=table_name, columns=columns, values=values)
 
   @staticmethod
-  def update(transaction, table_name, columns, values):
+  def update(transaction: spanner_transaction.Transaction, table_name: str,
+             columns: Iterable[str], values: Iterable[Iterable[Any]]) -> None:
     """Updates rows of a table."""
     _logger.debug('Update table=%s columns=%s values=%s', table_name, columns,
                   values)
     transaction.update(table=table_name, columns=columns, values=values)
 
   @staticmethod
-  def upsert(transaction, table_name, columns, values):
+  def upsert(transaction: spanner_transaction.Transaction, table_name: str,
+             columns: Iterable[str], values: Iterable[Iterable[Any]]) -> None:
     """Updates existing rows of a table or adds rows if they don't exist."""
     _logger.debug('Upsert table=%s columns=%s values=%s', table_name, columns,
                   values)
@@ -107,7 +125,7 @@ class SpannerApi(SpannerReadApi, SpannerWriteApi):
   _spanner_connection = None
 
   @classmethod
-  def _connection(cls):
+  def _connection(cls) -> spanner_database.SpannerDatabase:
     if not cls._spanner_connection:
       raise error.SpannerError('Not connected to spanner')
     return cls._spanner_connection
@@ -115,12 +133,12 @@ class SpannerApi(SpannerReadApi, SpannerWriteApi):
   # Spanner connection methods
   @classmethod
   def connect(cls,
-              instance,
-              database,
-              project=None,
-              credentials=None,
-              pool=None):
-    """Connects to the specified Spanner database."""
+              instance: str,
+              database: str,
+              project: Optional[str] = None,
+              credentials: Optional[auth_credentials.Credentials] = None,
+              pool: Optional[spanner.Pool] = None) -> None:
+    """Connects to the specified Spanner spanner_database."""
     connection_info = (instance, database, project, credentials)
     if cls._spanner_connection is not None:
       if connection_info == cls._connection_info:
@@ -133,6 +151,6 @@ class SpannerApi(SpannerReadApi, SpannerWriteApi):
     cls._connection_info = connection_info
 
   @classmethod
-  def hangup(cls):
+  def hangup(cls) -> None:
     cls._spanner_connection = None
     cls._connection_info = None
