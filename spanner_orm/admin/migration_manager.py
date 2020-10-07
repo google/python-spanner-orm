3af5eca9a82de6de607bf32c0aa81f86c8658afe
David Mandelberg <dseomn@google.com>
2020-10-07 18:24:57 -0400

    Fix pytype error when dynamically importing a python file.
    See https://github.com/google/pytype/issues/319 for context on the
    error.
    
    Addresses #93.

--- spanner_orm/admin/migration_manager.py
+++ spanner_orm/admin/migration_manager.py
@@ -72,10 +72,9 @@ class MigrationManager:
     """Loads a single migration from the given filename in the base dir."""
     module_name = re.sub(r'\W', '_', filename)
     path = os.path.join(self.basedir, filename)
-    spec = importlib.util.spec_from_file_location(module_name, path)
-    module = importlib.util.module_from_spec(spec)
-    # TODO(#93): Remove pytype disable below.
-    spec.loader.exec_module(module)  # type: ignore
+    module = importlib.util.module_from_spec(
+        importlib.util.spec_from_file_location(module_name, path))
+    importlib.machinery.SourceFileLoader(module_name, path).exec_module(module)
     try:
       result = migration.Migration(module.migration_id,
                                    module.prev_migration_id,
