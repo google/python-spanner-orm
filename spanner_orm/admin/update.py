9524002ad19fcc8a012263e61860adcc94f8bae2
David Mandelberg <dseomn@google.com>
2021-09-03 18:00:31 -0400

    Rely on Cloud Spanner to validate schema changes
    The model registry seems to be broken during migrations, because it
    assumes each model name will have only one corresponding class, but each
    migration that affects a model can create a new class. This commit fixes
    a bug where if there are any interleaved tables, accessing
    model_.interleaved while validating DropTable raises an exception
    because of the duplicate names.
    
    Instead of just fixing the validation code, I think it makes more sense
    to remove it entirely, since Cloud Spanner itself will raise exceptions
    about the same issues.

--- spanner_orm/admin/update.py
+++ spanner_orm/admin/update.py
@@ -55,9 +55,8 @@ class SchemaUpdate(MigrationUpdate, abc.ABC):
     self.validate()
     api.spanner_admin_api().update_schema(self.ddl())
 
-  @abc.abstractmethod
   def validate(self) -> None:
-    raise NotImplementedError
+    pass  # TODO(dseomn): Remove this method.
 
 
 class CreateTable(SchemaUpdate):
@@ -136,25 +135,6 @@ class DropTable(SchemaUpdate):
 
   def ddl(self) -> str:
     return 'DROP TABLE {}'.format(self._table)
-
-  def validate(self) -> None:
-    existing_model = metadata.SpannerMetadata.model(self._table)
-    if not existing_model:
-      raise error.SpannerError('Table {} does not exist'.format(self._table))
-
-    # Model indexes include the primary index
-    if len(existing_model.indexes) > 1:
-      raise error.SpannerError('Table {} has a secondary index'.format(
-          self._table))
-
-    self._validate_not_interleaved(existing_model)
-
-  def _validate_not_interleaved(self,
-                                existing_model: Type[model.Model]) -> None:
-    for model_ in metadata.SpannerMetadata.models().values():
-      if model_.interleaved == existing_model:
-        raise error.SpannerError('Table {} has interleaved table {}'.format(
-            self._table, model_.table))
 
 
 class AddColumn(SchemaUpdate):
